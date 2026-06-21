3d96ddc4e7c38bd14f8d11e647115c0762a82fcb
Daniel Gaulin <gaulindm@gmail.com>
2026-06-21 19:57:02 -0400

    Fixed navigation to exclude uke4ia

--- core/views.py
+++ core/views.py
@@ -14,18 +14,6 @@ def landing_page(request):
             "desc": "Connect and strum around the world",
             "icon": "bi-globe",
             "url": "strumsphere:home",
-        },
-        {
-            "name": "Uke4ia Performers",
-            "desc": "Manage your availability and be informed",
-            "icon": "bi-people-fill",
-            "url": "public_board",  # 👈 Goes to new grid dashboard
-        },
-        #        {
-        #    "name": "FrancontCube",
-        #    "desc": "Apprends à résoudre le Rubik’s Cube — version franco-ontarienne",
-        #    "icon": "bi-cube",
-        #    "url": "francontcube:home",   # Assuming you will make this namespace
-        #},
+        }
     ]
     return render(request, "core/landing.html", {"brands": brands})
